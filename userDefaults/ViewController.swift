4f8527f69a532c4b68bcd7df5a2ad06ea13b63c2
MRSgkce <mursidegokce985@gmail.com>
2024-10-11 15:57:34 +0300

    userDefault kullanarak küçük bir veri kayıt edildi ve silme denendi.

--- userDefaults/ViewController.swift
+++ userDefaults/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let ud = UserDefaults.standard
+        
+        // kayıt
+        ud.set("ahmet esat", forKey: "ad")
+        
+        //okuma
+        
+        let gelenAd = ud.string(forKey: "ad") ?? "kayıt yok"
+        print("gelen ad : \(gelenAd)")
+        
+        //silme
+        ud.removeObject(forKey:"ad")
+        
     }
 
 
